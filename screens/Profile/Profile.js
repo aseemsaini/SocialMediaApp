b73de573c087403c56b2529f4b42ec88afbb453d
Aseem Saini <aseemsaini@icloud.com>
2023-09-03 00:21:25 +0100

    Add Profile Page

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
+import globalStyle from '../../assets/styles/main';
+import style from './style';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Profile = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text> This is my Profile!</Text>
-      </View>
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView>
+        <View style={style.imageContainer}>
+          <View style={style.imageContent}>
+            <Image
+              source={require('../../assets/images/default_profile.png')}
+              style={style.image}
+            />
+          </View>
+        </View>
+        <View style={style.textContainer}>
+          <Text style={style.profileName}>Joel Miller</Text>
+        </View>
+        <View style={style.profileStatsContainer}>
+          <View style={[style.singleStatContainer, style.singleStatBorder]}>
+            <Text style={style.numberStat}>45</Text>
+            <Text style={style.statDefinition}>Following</Text>
+          </View>
+          <View style={[style.singleStatContainer, style.singleStatBorder]}>
+            <Text style={style.numberStat}>30M</Text>
+            <Text style={style.statDefinition}>Followers</Text>
+          </View>
+          <View style={style.singleStatContainer}>
+            <Text style={style.numberStat}>100</Text>
+            <Text style={style.statDefinition}>Posts</Text>
+          </View>
+        </View>
+        <View style={style.border} />
+        <View style={style.profileMediaContainer}>
+          <Text style={style.profileMediaOptions}>Photos</Text>
+          <Text style={style.profileMediaOptions}>Videos</Text>
+          <Text style={style.profileMediaOptions}>Saved</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ screens/Profile/style.js
@@ -0,0 +1,84 @@
+import {StyleSheet} from 'react-native';
+import {
+  horizontalScale,
+  scaltFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
+
+const style = StyleSheet.create({
+  imageContent: {
+    borderWidth: 1,
+    borderRadius: horizontalScale(100),
+    padding: horizontalScale(2),
+    borderColor: '#0150EC',
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: verticalScale(32),
+  },
+  image: {
+    width: horizontalScale(100),
+    height: horizontalScale(100),
+  },
+  textContainer: {
+    marginTop: verticalScale(20),
+  },
+  profileName: {
+    fontFamily: 'Inter',
+    color: '#022150',
+    fontSize: 20,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  profileStatsContainer: {
+    marginHorizontal: horizontalScale(24),
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    justifyContent: 'center',
+    marginVertical: verticalScale(20),
+  },
+  singleStatContainer: {
+    paddingHorizontal: horizontalScale(18),
+    alignItems: 'center',
+  },
+  singleStatBorder: {
+    borderRightWidth: 1,
+    borderColor: '#E9EFF1',
+  },
+  numberStat: {
+    fontFamily: 'Inter',
+    color: '#022150',
+    fontSize: scaltFontSize(20),
+    lineHeight: scaltFontSize(24),
+    fontWeight: '600',
+  },
+  statDefinition: {
+    color: '#79869F',
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: '400',
+    marginTop: verticalScale(5),
+  },
+  border: {
+    marginTop: verticalScale(10),
+    borderWidth: 1,
+    borderColor: '#EFF2F6',
+    marginHorizontal: horizontalScale(28),
+  },
+  profileMediaContainer: {
+    marginTop: verticalScale(32),
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'space-between',
+  },
+  profileMediaOptions: {
+    color: '#022150',
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
+
+export default style;
